1b9a6e543698f8f9c28686fd2bf822e211d8e13a
caojianjin <admin@Mac-mini.local>
2023-06-13 21:34:29 +0800

    命令模式

--- DesignModeDemo/DesignModeDemo/行为型/命令模式/Command.swift
+++ DesignModeDemo/DesignModeDemo/行为型/命令模式/Command.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+
+protocol Command {
+    func excute()
+}
+
+class Invoker {
+    let command: Command
+    init(_ command: Command) {
+        self.command = command
+    }
+    func invoke() {
+        command.excute()
+    }
+}
+
+class WashClothesCommand: Command {
+    func excute() {
+        Receiver1().washClothes()
+    }
+}
+class SweepCommand: Command {
+    func excute() {
+        Receiver2().sweep()
+    }
+}
+
+class CleanCommand: Command {
+    func excute() {
+        Receiver3().clean()
+    }
+}
+
+
+class Receiver1 {
+    func washClothes() {
+        print("洗衣服")
+    }
+}
+
+class Receiver2 {
+    func sweep() {
+        print("扫地")
+    }
+}
+
+class Receiver3 {
+    func clean() {
+        print("搞卫生")
+    }
+}
